31a80fea52f751acce3ae2cdcd20bd7b1b345d17
Ingebrigt <ing.hovind@hotmail.com>
2021-02-09 12:43:01 +0100

    springboot oeving2

---
+++ Oevinger/Oeving 2/src/main/java/ntnu/idatt2105/boteam3/obl2/oeving2/Oeving2Application.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2105.boteam3.obl2.oeving2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oeving2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oeving2Application.class, args);
+	}
+
+}
