e6eab3325d7699bf67e943ba275655e865429b9c
erlinssl <erlinss@stud.ntnu.no>
2021-02-09 12:50:16 +0100

    Hello world implemented

--- Oevinger/Oeving 2/src/main/java/ntnu/idatt2105/boteam3/obl2/oeving2/Oeving2Application.java
+++ Oevinger/Oeving 2/src/main/java/ntnu/idatt2105/boteam3/obl2/oeving2/Oeving2Application.java
@@ -2,12 +2,20 @@ package ntnu.idatt2105.boteam3.obl2.oeving2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Oeving2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oeving2Application.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
