1a61bb2b0378d5f0b3e4d4546abca1eefd0054c9
Ingebrigt <ing.hovind@hotmail.com>
2021-02-15 21:47:02 +0100

    started jdbc exercise

---
+++ JDBC exercise/src/main/java/Main.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+        StudentJDBCTemplate studentJDBCTemplate = (StudentJDBCTemplate)
+                context.getBean("studentJDBCTemplate");
+
+        System.out.println("------Records Creation--------" );
+        studentJDBCTemplate.create("Zara", 11);
+        studentJDBCTemplate.create("Nuha", 2);
+        studentJDBCTemplate.create("Ayan", 15);
+
+        System.out.println("------Listing Multiple Records--------" );
+        List<Student> students = studentJDBCTemplate.listStudents();
+
+        for (Student record : students) {
+            System.out.print("ID : " + record.getId() );
+            System.out.print(", Name : " + record.getName() );
+            System.out.println(", Age : " + record.getAge());
+        }
+    }
+}
